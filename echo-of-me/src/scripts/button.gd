454fc0a87b2b9eef6153a7b2f00f1df73fbd0610
Magnus Dybdal <magndy@stud.ntnu.no>
2025-09-29 14:30:51 +0200

    reset signal on button and platform

--- echo-of-me/src/scripts/button.gd
+++ echo-of-me/src/scripts/button.gd
@@ -2,14 +2,21 @@ extends Area2D
 
 @export var is_pressed: bool = false
 
+var start_position : Vector2
+
 signal pressed
 
 func _ready():
 	$AnimatedSprite2D.play("released")
+	var level_controller = get_tree().current_scene.get_node("LevelController")
+	level_controller.connect("reset_level", Callable(self, "_on_reset_level"))
 	
-
 func _on_body_entered(_body: Node2D) -> void:
 	if not is_pressed:
 		is_pressed = true
 		$AnimatedSprite2D.play("pressed")
 		emit_signal("pressed")
+		
+func _on_reset_level():
+	$AnimatedSprite2D.play("released")
+	is_pressed = false
--- echo-of-me/src/scripts/platform.gd
+++ echo-of-me/src/scripts/platform.gd
@@ -1,5 +1,17 @@
 extends AnimatableBody2D
 
+var start_position : Vector2
+
+func _ready():
+	start_position = global_position
+	var level_controller = get_tree().current_scene.get_node("LevelController")
+	level_controller.connect("reset_level", Callable(self, "_on_reset_level"))
+	
+func _on_reset_level():
+	global_position = start_position
+	$Rise.stop()
+
+
 func rise():
 	$Rise.play("rise")
 
