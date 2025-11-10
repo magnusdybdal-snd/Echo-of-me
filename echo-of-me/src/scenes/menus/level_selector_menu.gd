ec9eaffeee62c92141ef72a9e6f622c990fd5fdd
maeilert <maeilert@stud.ntnu.no>
2025-11-10 12:55:19 +0100

    main menu and basic level menu

---
+++ echo-of-me/src/scenes/menus/level_selector_menu.gd
@@ -0,0 +1,60 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_level_0_pressed() -> void:
+	pass # Replace with function body.
+	
+
+
+func _on_level_7_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_level_8_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_level_9_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_level_10_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_level_11_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_level_1_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_level_2_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_level_3_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_level_4_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_level_5_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_level_6_pressed() -> void:
+	pass # Replace with function body.
