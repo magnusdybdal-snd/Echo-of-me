bcfbaae6359652859874c56b572fb45d5191c9a1
maeilert <maeilert@stud.ntnu.no>
2025-11-10 13:29:38 +0100

    play button loads level_01 now

--- echo-of-me/src/scripts/main_menu.gd
+++ echo-of-me/src/scripts/main_menu.gd
@@ -12,7 +12,7 @@ func _process(delta: float) -> void:
 
 
 func _on_start_game_pressed() -> void:
-	get_tree().change_scene_to_file("res://src/scenes/levels/test_level.tscn")
+	get_tree().change_scene_to_file("res://src/scenes/levels/level_01.tscn")
 
 
 func _on_select_level_pressed() -> void:
