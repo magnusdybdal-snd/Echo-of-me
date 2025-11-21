6e4272da1075ff5a219133de57e8a7b95cb3fc85
maeilert <maeilert@stud.ntnu.no>
2025-11-21 15:40:22 +0100

    main menu script revert & paused menu text

--- echo-of-me/src/scripts/pause_menu.gd
+++ echo-of-me/src/scripts/pause_menu.gd
@@ -9,15 +9,22 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		get_tree().paused = false
 
 
-func _on_start_game_pressed() -> void:
-	get_tree().change_scene_to_file("res://src/scenes/levels/level_01.tscn")
+func _on_continue_game_pressed() -> void:
+	get_tree().paused = false
+	queue_free() 
 
 
 func _on_select_level_pressed() -> void:
+	get_tree().paused = false
 	get_tree().change_scene_to_file("res://src/scenes/menus/level_selector_menu.tscn")
 
 
-func _on_quit_game_pressed() -> void:
-	get_tree().quit()
+func _on_main_menu_pressed() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://src/scenes/menus/main_menu.tscn")
