56c8fac395956ccbec601ae38902aedb2dac8c89
maeilert <maeilert@stud.ntnu.no>
2025-11-24 11:48:09 +0100

    instruction label logic for boxes

--- echo-of-me/src/scripts/game_instructions.gd
+++ echo-of-me/src/scripts/game_instructions.gd
@@ -15,10 +15,15 @@ func update_instructions() -> void:
 	var echo_label = $"Overlay Box/HBoxContainer/Echo"
 	var reset_label = $"Overlay Box/HBoxContainer/Reset"
 	var dash_label = $"Overlay Box/HBoxContainer/Dash"
+	var pickup_label = $"Overlay Box/HBoxContainer/PickUp"
+
+	# Check if there are any boxes in the current scene
+	var has_boxes = get_tree().get_nodes_in_group("boxes").size() > 0
 
 	# Show/hide based on GameManager state
 	run_label.visible = GameManager.has_powerup("sprint")
 	echo_label.visible = GameManager.can_use_echoes()
 	reset_label.visible = GameManager.can_use_echoes()
 	dash_label.visible = GameManager.has_powerup("dash")
+	pickup_label.visible = has_boxes
 	
