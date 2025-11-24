e8b254d28ecdf1b9715432d0fbc178a5d4953735
maeilert <maeilert@stud.ntnu.no>
2025-11-24 14:33:17 +0100

    basic settings screen

--- echo-of-me/src/scripts/pause_menu.gd
+++ echo-of-me/src/scripts/pause_menu.gd
@@ -19,11 +19,13 @@ func _on_continue_game_pressed() -> void:
 	get_tree().paused = false
 	queue_free() 
 
-
 func _on_select_level_pressed() -> void:
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://src/scenes/menus/level_selector_menu.tscn")
 
+func _on_settings_pressed() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://src/scenes/menus/settings_menu.tscn")
 
 func _on_main_menu_pressed() -> void:
 	get_tree().paused = false
