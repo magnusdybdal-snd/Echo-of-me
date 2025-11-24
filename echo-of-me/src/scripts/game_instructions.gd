083f38e2c1f336f8631369b7587feee71c59cf20
maeilert <maeilert@stud.ntnu.no>
2025-11-24 11:44:03 +0100

    label display logic

--- echo-of-me/src/scripts/game_instructions.gd
+++ echo-of-me/src/scripts/game_instructions.gd
@@ -3,13 +3,22 @@ extends CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	update_instructions()
 
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
 		get_tree().paused = false
+
+func update_instructions() -> void:
+	# Get references to labels
+	var run_label = $"Overlay Box/HBoxContainer/Run"
+	var echo_label = $"Overlay Box/HBoxContainer/Echo"
+	var reset_label = $"Overlay Box/HBoxContainer/Reset"
+	var dash_label = $"Overlay Box/HBoxContainer/Dash"
+
+	# Show/hide based on GameManager state
+	run_label.visible = GameManager.has_powerup("sprint")
+	echo_label.visible = GameManager.can_use_echoes()
+	reset_label.visible = GameManager.can_use_echoes()
+	dash_label.visible = GameManager.has_powerup("dash")
+	
