eef44a38c23c3327310767b76cae438f1e5eb1bc
Magnus Dybdal <magndy@stud.ntnu.no>
2025-11-24 11:16:33 +0100

    added cutscene level to meta data

--- echo-of-me/src/scripts/level_selector_menu.gd
+++ echo-of-me/src/scripts/level_selector_menu.gd
@@ -12,7 +12,7 @@ func _connect_level_buttons() -> void:
 					button.pressed.connect(_on_level_button_pressed.bind(button))
 
 func _on_level_button_pressed(button: Button) -> void:
-	var level_index = 0 if button.name == "test_level" else int(button.name)
+	var level_index = 0 if button.name == "test_level" else int(button.name) + 1
 	GameManager.load_level(level_index)
 
 ###--- Check if level exists---###
