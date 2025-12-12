33f603a8ba1a1753d31184335aa3956ee46c4e50
Magnus Dybdal <magndy@stud.ntnu.no>
2025-12-12 20:39:08 +0100

    completed structure of level, design left

--- echo-of-me/src/scripts/sliding_door.gd
+++ echo-of-me/src/scripts/sliding_door.gd
@@ -1,28 +1,24 @@
 extends Node2D
 
+enum SlideDirection  { LEFT, RIGHT }
+enum DoorType { NORMAL, TIMER }
+
 @onready var anim_player: AnimationPlayer = $AnimationPlayer
 @onready var platform_body: AnimatableBody2D = $AnimatableBody2D
+
+@export var slide_direction : SlideDirection = SlideDirection.RIGHT
+@export var door_type: DoorType = DoorType.NORMAL
+@export var anim_speed_scale: float = 10.0 # Standard for fast open/close doors
 
 var start_position: Vector2
 
 func _ready():
-	# Debug: Print what children we have
-	print("=== Sliding Door Debug ===")
-	print("Children of ", name, ":")
-	for child in get_children():
-		print("  - ", child.name, " (type: ", child.get_class(), ")")
-
-	# Ensure nodes are ready
-	if not platform_body:
-		push_error("AnimatableBody2D not found! Check scene structure.")
-		return
-
-	if not anim_player:
-		push_error("AnimationPlayer not found!")
-		return
-
 	# Store the starting position of the platform body (should be 0,0 relative to parent)
 	start_position = platform_body.position
+	
+	# Sets the animation speed scale of the door
+	if anim_player:
+		anim_player.speed_scale = anim_speed_scale
 
 	# Connect to level reset
 	var level_controller = get_tree().current_scene.get_node("LevelController")
@@ -30,14 +26,29 @@ func _ready():
 		level_controller.connect("reset_level", Callable(self, "_on_reset_level"))
 
 func _on_button_pressed():
-	print("Sliding door: Button pressed - opening")
-	if anim_player and anim_player.has_animation("Slide"):
-		anim_player.play("Slide")
+	if anim_player:
+		match door_type:
+			DoorType.NORMAL:
+				match slide_direction:
+					SlideDirection.RIGHT:
+						anim_player.play("SlideRight")
+					SlideDirection.LEFT:
+						anim_player.play("SlideLeft")
+			DoorType.TIMER:
+				anim_player.play("TimerOpen")
+
 
 func _on_button_released():
-	print("Sliding door: Button released - closing")
-	if anim_player and anim_player.has_animation("Slide"):
-		anim_player.play_backwards("Slide")
+	if anim_player:
+		match door_type:
+			DoorType.NORMAL:
+				match slide_direction:
+					SlideDirection.RIGHT:
+						anim_player.play_backwards("SlideRight")
+					SlideDirection.LEFT:
+						anim_player.play_backwards("SlideLeft")
+			DoorType.TIMER:
+				anim_player.play("TimerClose")
 
 func _on_reset_level():
 	print("Sliding door: Resetting")
