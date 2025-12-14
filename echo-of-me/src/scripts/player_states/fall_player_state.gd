1acdcc4439d19486f75b36a1bba37a22be1be381
Magnus Dybdal <magndy@stud.ntnu.no>
2025-12-14 09:12:41 +0100

    reimplemented double jump

--- echo-of-me/src/scripts/player_states/fall_player_state.gd
+++ echo-of-me/src/scripts/player_states/fall_player_state.gd
@@ -17,6 +17,9 @@ func pre_update(player: CharacterBase) -> void:
 	if player.is_action_just_pressed_virtual("dash") and not player.has_used_dash:
 		player.change_state_to(PlayerStates.DASH)
 		return
+		
+	if player.is_action_just_pressed_virtual("jump") and player.can_double_jump and not player.used_double_jump:
+		player.change_state_to(PlayerStates.JUMP)
 
 
 func update(player: CharacterBase, delta: float) -> void:
--- echo-of-me/src/scripts/player_states/jump_player_state.gd
+++ echo-of-me/src/scripts/player_states/jump_player_state.gd
@@ -5,7 +5,11 @@ const JUMP_VELOCITY := -370.0
 const SECOND_JUMP_VELOCITY := -270
 
 func enter(player: CharacterBase) -> void:
-	player.velocity.y = JUMP_VELOCITY if player.is_on_floor() else SECOND_JUMP_VELOCITY
+	if player.is_on_floor():
+		player.velocity.y = JUMP_VELOCITY
+	else:
+		player.velocity.y = SECOND_JUMP_VELOCITY
+		player.used_double_jump = true
 	player.animated_sprite.play("jump")
 	AudioPlayer.play_sfx("jump")
 
@@ -21,6 +25,9 @@ func pre_update(player: CharacterBase) -> void:
 	if player.is_action_just_pressed_virtual("dash") and not player.has_used_dash:
 		player.change_state_to(PlayerStates.DASH)
 		return
+		
+	if player.is_action_just_pressed_virtual("jump") and player.can_double_jump and not player.used_double_jump:
+		player.change_state_to(PlayerStates.JUMP)
 
 
 func update(player: CharacterBase, delta: float) -> void:
